0ac5e7f8e384dec0f5b212d6e956aa84c498eebf
rtsp <git@rtsp.us>
2025-03-25 03:03:40 +0700

    Change MODE 65 to AUTO instead of HEAT/COOL

--- components/toshiba_suzumi/toshiba_climate_mode.h
+++ components/toshiba_suzumi/toshiba_climate_mode.h
@@ -31,7 +31,7 @@ static const std::string &SPECIAL_MODE_SILENT_2 = "Silent#2";
 enum class CustomFanModes { QUIET, LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5, AUTO };
 
 // codes as reverse engineered from Toshiba AC communication with original Wifi module.
-enum class MODE { HEAT_COOL = 65, COOL = 66, HEAT = 67, DRY = 68, FAN_ONLY = 69 };
+enum class MODE { AUTO = 65, COOL = 66, HEAT = 67, DRY = 68, FAN_ONLY = 69 };
 enum class FAN {
   QUIET = 49,
   FANMODE_1 = 50,
